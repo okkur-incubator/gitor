a89b37ab6cd624898150b82b4fdc6bb4c959b129
David Riesz <dbriesz@users.noreply.github.com>
2017-10-18 13:16:25 -0700

    Add help function

--- main.go
+++ main.go
@@ -2,6 +2,8 @@ package main
 
 import (
 	"flag"
+	"fmt"
+	"os"
 )
 
 func main() {
@@ -13,10 +15,18 @@ func main() {
 
 	flag.StringVar(&upstream, "upstream", "https://github.com/okkur/gitor.git", "specifies upstream")
 	flag.StringVar(&branch, "branch", "master", "specifies branch")
+	flag.Usage = usage
 
 	flag.Parse()
 	command := flag.Args()[0]
 	if command == "pull" {
 		pull(command, upstream, branch)
 	}
+}
+
+func usage() {
+	fmt.Fprintf(os.Stderr, "usage: gitor [subcommand] [flags]\n")
+	flag.PrintDefaults()
+	fmt.Fprintf(os.Stderr, "Subcommands: pull\n")
+	os.Exit(2)
 }
