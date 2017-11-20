8e3bcc5617f36837d6b0b6f1efbb4d88b07fc90e
David Riesz <dbriesz@users.noreply.github.com>
2017-11-20 15:06:07 -0800

    Add ability to set branch

--- update.go
+++ update.go
@@ -20,6 +20,8 @@ import (
 	"strconv"
 	"strings"
 
+	"gopkg.in/src-d/go-git.v4/plumbing"
+
 	"gopkg.in/src-d/go-git.v4/plumbing/transport"
 
 	git "gopkg.in/src-d/go-git.v4"
@@ -27,7 +29,7 @@ import (
 	"gopkg.in/src-d/go-git.v4/storage/memory"
 )
 
-func update(upstream string, branch string, downstream string,
+func update(upstream string, upstreamRef string, downstream string, downstreamRef string,
 	upstreamAuth transport.AuthMethod, downstreamAuth transport.AuthMethod) error {
 
 	// Validate upstream URL
@@ -64,14 +66,14 @@ func update(upstream string, branch string, downstream string,
 		}
 	}
 
-	pull(r, upstream, upstreamAuth)
+	pull(r, upstream, upstreamRef, upstreamAuth)
 
-	push(r, downstream, downstreamAuth)
+	push(r, downstream, upstreamRef, downstreamRef, downstreamAuth)
 
 	return nil
 }
 
-func pull(r *git.Repository, upstream string, upstreamAuth transport.AuthMethod) {
+func pull(r *git.Repository, upstream string, upstreamRef string, upstreamAuth transport.AuthMethod) {
 	// Get the working directory for the repository
 	w, err := r.Worktree()
 	if err != nil {
@@ -81,9 +83,14 @@ func pull(r *git.Repository, upstream string, upstreamAuth transport.AuthMethod)
 	// Pull using default options
 	// If authentication required pull using authentication
 	log.Printf("Pulling %s ...\n", upstream)
+
+	var reference plumbing.ReferenceName
+	reference = plumbing.ReferenceName(upstreamRef)
+
 	err = w.Pull(&git.PullOptions{
-		RemoteName: upstreamDefaultRemoteName,
-		Auth:       upstreamAuth,
+		RemoteName:    upstreamDefaultRemoteName,
+		ReferenceName: reference,
+		Auth:          upstreamAuth,
 	})
 	if err != nil {
 		switch err {
@@ -108,7 +115,7 @@ func pull(r *git.Repository, upstream string, upstreamAuth transport.AuthMethod)
 	log.Printf("Pulled: %s\n", commit.Hash)
 }
 
-func push(r *git.Repository, downstream string, downstreamAuth transport.AuthMethod) {
+func push(r *git.Repository, downstream string, upstreamRef string, downstreamRef string, downstreamAuth transport.AuthMethod) {
 	// Validate downstream URL
 	err := validateRepo(downstream, downstreamAuth)
 	if err != nil {
@@ -131,9 +138,11 @@ func push(r *git.Repository, downstream string, downstreamAuth transport.AuthMet
 
 	// Push using default options
 	// If authentication required push using authentication
+	referenceList := append([]config.RefSpec{}, config.RefSpec(upstreamRef+":"+downstreamRef))
 	log.Printf("Pushing to %s ...\n", downstream)
 	err = r.Push(&git.PushOptions{
 		RemoteName: downstreamDefaultRemoteName,
+		RefSpecs:   referenceList,
 		Auth:       downstreamAuth,
 	})
 	if err != nil {
